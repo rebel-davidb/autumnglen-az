7656f8333e3240bed3ee863c1d0ba2abb4fbc3d9
David Benoit <dbenoit@rebelliongroup.com>
2026-06-03 05:12:21 -0400

    mobile hero home

--- .eleventy.js
+++ .eleventy.js
@@ -13,6 +13,8 @@ module.exports = function (eleventyConfig) {
 
   eleventyConfig.addPassthroughCopy({ "src/assets": "assets" });
   eleventyConfig.addPassthroughCopy("src/favicon.svg");
+  eleventyConfig.addPassthroughCopy("src/favicon.ico");
+  eleventyConfig.addPassthroughCopy("src/site.webmanifest");
   eleventyConfig.addPassthroughCopy("src/robots.txt");
   eleventyConfig.addPassthroughCopy("src/_redirects");
   eleventyConfig.addPassthroughCopy("src/forms.html");
